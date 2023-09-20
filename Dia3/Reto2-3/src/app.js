9beee152271aa1762a0fc6629c49ab384ec62064
Kiskotkd <fjgp92@hotmail.com>
2023-09-20 10:07:03 +0200

    reto api

--- Dia3/Reto2-3/src/app.js
+++ Dia3/Reto2-3/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const errorHandling = require('./error/errorHandling');
 const bookRouters = require("./routers/book.routers");
+const booksRouters = require("./routers/books.routers")
 
 const app = express();
 
@@ -10,7 +11,8 @@ app.set("port", process.env.PORT || 3000)
 app.use(cors());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(bookRouters)
+app.use(bookRouters);
+app.use(booksRouters);
 app.use((req, res, next) => {
     res.status(404).json({
         error:true,
