7101cf5dd7260eac7a3542c35ec5be55baf1e0c1
Kiskotkd <fjgp92@hotmail.com>
2023-09-20 12:17:55 +0200

    getid

--- Dia3/Reto2-3/src/controller/books.controller.js
+++ Dia3/Reto2-3/src/controller/books.controller.js
@@ -2,16 +2,12 @@ const express = require("express");
 const app = express();
 const Book = require('../models/book');
 
-let books = null;
+let books = [new Book("300 recetas sencillas", "cocina", "Arguillano", 20, "aqui hay foto", 1, 0),
+            new Book("100 recetas medias", "cocinando", "Arguillano jr", 20, "aqui hay foto", 22, 3)];
 
 const getbookid = (req, res) =>{
-    let getbook;
-    books.getbookid.forEach((book,i)=>{
-        if(req.body.id_book == book.id_book){
-            books.slice(i,1);
-            getbook = true;
-        }
-    })
+    let getbook = books.find(book => book.id_book = req.query.id_book)
+    console.log(getbook)
 
     if(getbook){ res.send("Libro encontrado")
 } else {res.status(404).send({error: true, codigo: 404, mensaje: "Libro no encontrado"})}
@@ -32,10 +28,10 @@ const postbooks = (req,res) => {
     console.log(req.book);
     if(books === null){
         books = new Book (req.body.title, req.body.type, req.body.author, req.body.price, req.body.photo, req.body.id_book, req.body.id_user);
-        respuesta = {error:false, codigo:404,
+        respuesta = {error:false, codigo:200,
                         mensaje:'Libro creado', resultado: book};}
     else
-        respuesta = {error:false, codigo:404,
+        respuesta = {error:true, codigo:404,
                     mensaje:'El libro ya existe', resultado: null};
     
     res.send(respuesta);
@@ -58,7 +54,7 @@ const putbooks = (req, res) =>{
 const deletebooks = (req, res) =>{
     let delbook = false;
 
-    books.deletebooks.forEach((book,i) => {
+    books.forEach((book,i) => {
         if(req.body.id_book == book.id_book){
             books.splice(i,1);
             delbook = true;
